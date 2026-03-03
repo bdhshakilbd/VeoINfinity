2943737b3fdc9721f4e1972b4dfc576b487b6c12
bdhshakilbd <bdhshakilbd@gmail.com>
2026-03-03 20:15:29 +0600

    Remove all source files - keep only installer EXE (v3.8.2)

--- android/app/src/main/kotlin/com/example/veo3_another/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.veo3_another
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/models/poll_request.dart
+++
@@ -1,5 +0,0 @@
-class PollRequest {
-  final String operationName;
-  final String sceneId;
-  PollRequest(this.operationName, this.sceneId);
-}
